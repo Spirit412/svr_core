c9bed3811ba1685553f62b9c918c7592c8782fd0
Aleksandr Domanskiy <spirit412@gmail.com>
2024-10-17 15:21:14 +0300

    edit composer.json and seeders 1.0.24

--- database/seeders/table_data_system_modules.php
+++ database/seeders/table_data_system_modules.php
@@ -3,7 +3,6 @@
 namespace Svr\Core\Seeders;
 
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
--- database/seeders/table_data_system_roles.php
+++ database/seeders/table_data_system_roles.php
@@ -2,7 +2,6 @@
 
 namespace Svr\Core\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
